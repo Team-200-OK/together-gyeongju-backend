2451b849690c6dfeb7f91c990d87c933018eebd2
minnnisu <mine52100@naver.com>
2024-11-30 17:02:30 +0900

    init: 백엔드 프로젝트 초기화

---
+++ src/main/java/org/team200ok/togethergyeongju/TogetherGyeongjuApplication.java
@@ -0,0 +1,13 @@
+package org.team200ok.togethergyeongju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogetherGyeongjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogetherGyeongjuApplication.class, args);
+	}
+
+}
